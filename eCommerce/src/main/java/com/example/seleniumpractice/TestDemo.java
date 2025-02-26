6803352438a7c249fa13c30079df5127a75b3a8a
Raviprasad <78257726+raviprasadchowdhary@users.noreply.github.com>
2025-02-26 10:05:16 +0530

    initial commit

---
+++ eCommerce/src/main/java/com/example/seleniumpractice/TestDemo.java
@@ -0,0 +1,17 @@
+package com.example.seleniumpractice;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestDemo {
+
+	public static void main(String[] args) {
+		
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://rahulshettyacademy.com/seleniumPractise/");
+		
+		
+
+	}
+
+}
